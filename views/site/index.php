d893a1ab62e2b6c434c27b38902984362691b9d1
pengxl <pengxl1999@126.com>
2019-05-28 21:45:27 +0800

    v0.89

--- views/site/index.php
+++ views/site/index.php
@@ -29,24 +29,21 @@ $this->title = 'Vending Machine';
                     获取更多信息，请点击此按钮。
                 </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"> 详细使用说明&raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"> 详细使用说明 &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>关于语音智能药品售货机</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>近年来，医疗体系在不断完善，零售药店也在向以互联网为核心的智慧医疗模式转变。针对现今零售药店执业药师不足的问题，TO Group采用物联网技术研发了一款基于语音交互的智能售药机。</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">更多 &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>关于我们</h2>
 
                 <p>TO Group of Jilin University是吉林大学计算机科学与技术学院无线传感器网络实验室物联网设备小组，</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">联系我们 &raquo;</a></p>
             </div>
         </div>
 
