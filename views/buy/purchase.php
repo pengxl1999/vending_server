24a3f85083fdfe13c5ab9ca17d5f18e31474ed2a
pengxl <pengxl1999@126.com>
2019-05-30 14:59:57 +0800

    v0.94

--- views/buy/purchase.php
+++ views/buy/purchase.php
@@ -94,8 +94,10 @@ $this->params['breadcrumbs'][] = $this->title;
                                     ['class' => "btn btn-sm btn-success",
                                         'style' => 'font-size:xx-small']);
                             case \app\models\AppointmentStatus::$ALREADY_PAID:
-                                return Html::a('取货码', null, ['class' => "btn btn-sm btn-success",
-                                    'style' => 'font-size:xx-small', 'onclick' => "window.android.createQRCode()"]);
+                                return '
+                                    <a class="btn btn-danger" onclick="window.android.createQRCode(
+                                        \" ' . $model->ca_order . '\" , ' . $model->m_id . ',' . $model->num . ')">取货码</a>
+                                    ';
                             case \app\models\AppointmentStatus::$ALREADY_FINISHED:
                                 return null;
                             case \app\models\AppointmentStatus::$TIME_OUT:
