5b29f7d0931b7af8870d51ce473cd372ad608798
pengxl <pengxl1999@126.com>
2019-06-02 16:07:58 +0800

    v0.96

--- controllers/BuyController.php
+++ controllers/BuyController.php
@@ -435,10 +435,7 @@ class BuyController extends Controller
                $appointment->status = AppointmentStatus::$TIME_OUT;
                $appointment->save();
             }
-            echo strtotime(date("Y-m-d H:i:s"));
-            echo "\n";
-            echo date_timestamp_get(date_create($appointment->deadline));
-            echo "\n";
+            echo (strtotime(date("Y-m-d H:i:s")) > date_timestamp_get(date_create($appointment->deadline)));
             if(strtotime(date("Y-m-d H:i:s")) > date_timestamp_get(date_create($appointment->deadline))
                 && $appointment->status == AppointmentStatus::$ALREADY_PAID) {
                 $appointment->status = AppointmentStatus::$DEADLINE_EXCEED;
